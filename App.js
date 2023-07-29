85cf1297ef60b99ca2371860745d635485fd1b72
aparna2903 <aparnagavali61@gmail.com>
2023-07-29 20:07:24 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Assi1 from "./Assi1";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Random background Change </h1> */}
+      <Assi1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ Assi1.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+function Assi1() {
+  const handleClickFunction = (e) => {
+    const body = document.querySelector("body");
+    body.style.background = getRandomColor();
+    e.target.style.backgroundColor = getRandomColor();
+  };
+
+  function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  return (
+    <>
+      <div className="container">
+        <h1>Random Background Color Changer</h1>
+        <button className="btn" onClick={handleClickFunction}>
+          Click Me
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Assi1;
